21cf4a64cce9b07f64d52764a365598b774efaf8
Pavel Mishanin <pavelmishanin@mail.ru>
2022-05-25 17:26:25 +0700

    main: Create project

---
+++ MVVM-RxSwift/MVVM-RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-RxSwift
+//
+//  Created by pavel mishanin on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
